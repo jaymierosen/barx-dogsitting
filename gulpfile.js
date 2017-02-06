3b7702419e9e6d7ecdf74d6023393dfe2843c63a
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-02-05 21:43:10 -0500

    git ignore

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
   return gulp.src('./dev/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(concat('./public/styles/styles.css'))
     .pipe(gulp.dest('./public/styles'))
 });
