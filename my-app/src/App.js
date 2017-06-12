77093aaf2e69f2cb4f7353f5d51d7a17975e886b
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-06-11 21:11:05 -0400

    finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,15 +16,16 @@ class App extends Component {
                         <h1>barx</h1>
                         <h2>dog sitting services <span role='img' aria-label='dog'>🐶</span></h2>
                     </hgroup>
-                    <p>Hello there! My name is Jaymie, and I live in Toronto. The building I reside in has many dog owners (in fact, 70% of the people who live in my building own dogs!). I'm very much interested in looking </p>
+                    <p>Hello there! My name is Jaymie, and I live in Toronto. I am interested in part-time dogsitting for any dogs who live at 44 jackes avenue. as a former dog owner, i am patient, loving and attentive. i have a clean, spacious apartment that can host up to <strong>one</strong> dog at a time. i can provide walks, and adventures to the park, feedings, and.</p>
                 </header>
             </section>
             <section className='services'>
                 <h3>services <span role='img' aria-label='pencil and paper'>📝</span></h3>
                 <ul className='servicesList'>
-                    <li><strong>short-term dogsitting:</strong> 1 - 2 days; </li>
-                    <li><strong>long-term dogsitting:</strong> 3 or more days; </li>
+                    <li><strong>short-term dogsitting:</strong> 1 - 2 days; evenings or weekends only.</li>
+                    <li><strong>long-term dogsitting:</strong> 2 or more days; evenings or weekends only.</li>
                     <li><strong className='note'>*Note: </strong>I need at least <strong>72 hours</strong> notice for any bookings. exceptions can be made for&nbsp;emergencies.</li>
+                    <li><strong className='note'>*Note: </strong>I am currently unavailable Tuesday and Thursday evenings, as well as Saturday mornings/early afternoons.</li>
                 </ul>
             </section>
             <section className='contact'>
@@ -39,9 +40,9 @@ class App extends Component {
                         <input type="email" name="Email Address" id="emailAddress" required></input>
                     <label htmlFor="phoneNumber">Your Phone Number</label>
                         <input type="text" name="Phone Number" id="phoneNumber" required></input>
-                    <label htmlFor="shortTerm">Short Term Dogsitting (1 - 2 days)</label>
+                    <label htmlFor="shortTerm">Short Term Dogsitting (1 - 2 days; evenings or weekends only)</label>
                         <input type="radio" name="service" id="shortTerm" value="short term"></input>
-                    <label htmlFor="longTerm" className='service'>Long Term Dogsitting (3 or more days)</label>
+                    <label htmlFor="longTerm" className='service'>Long Term Dogsitting (2 or more days; evenings or weekends only)</label>
                         <input type="radio" name="service" id="longTerm" value="long term"></input>
                     <label htmlFor="startDate" className='service'>Start Date</label>
                         <input name="startDate" type="date" id="startDate" required></input>
